7485abdfefc7569fd1fa74babdbefb11c28aa721
Qinghuai Zhou(周清怀) <qinghuai.zhou@msxf.com>
2020-05-14 15:37:38 +0800

    使用插件

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-import foo from './foo.js';
+// import foo from './foo.js';
+// export default function () {
+//   console.log(foo);
+// }
+
+// src/main.js
+import { version } from '../package.json';
+
 export default function () {
-  console.log(foo);
+  console.log('version ' + version);
 }
